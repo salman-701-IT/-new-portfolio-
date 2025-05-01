6418d78ce32308b628b00f6f7fe88c9c090172aa
Salman Khan <salmankhan701.it@gmail.com>
2025-05-01 18:25:12 +0000

    // components/ThreeScene.tsx 'use client'; import { Canvas } from '@react-three/fiber';
    export default function ThreeScene() {
      return (
        <Canvas>
          {/* your 3D scene here */}
        </Canvas>
      );
    }

---
+++ components/ThreeScene.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { Canvas } from '@react-three/fiber';
+
+export default function ThreeScene() {
+  return (
+    <Canvas>
+      {/* your 3D scene here */}
+    </Canvas>
+  );
+}
